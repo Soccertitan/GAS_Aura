32e45d36ef2cf99e59b243766ef4dce22246ea28
Soccertitan <tristan.nowacki@outlook.com>
2023-11-14 17:28:36 -0800

    Activating the Enemy Hit React Ability

--- Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -43,5 +43,8 @@ public:
 	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
 	TSubclassOf<UGameplayEffect> VitalAttributes;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TArray<TSubclassOf<UGameplayAbility>> CommonAbilities;
+	
 	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
 };
