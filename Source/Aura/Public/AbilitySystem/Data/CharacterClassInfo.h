c21e8d09184e8a933226d31f9933aabaaf556493
Soccertitan <tristan.nowacki@outlook.com>
2023-11-16 10:36:26 -0800

    Damage Calculation Coefficients

--- Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -6,6 +6,7 @@
 #include "Engine/DataAsset.h"
 #include "CharacterClassInfo.generated.h"
 
+class UGameplayAbility;
 class UGameplayEffect;
 
 UENUM(BlueprintType)
@@ -45,6 +46,9 @@ public:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
 	TArray<TSubclassOf<UGameplayAbility>> CommonAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults|Damage")
+	TObjectPtr<UCurveTable> DamageCalculationCoefficients;
 	
 	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
 };
