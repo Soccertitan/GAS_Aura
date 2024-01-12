be0dab70a504252f63a4b399a07b76b940c7a84e
Soccertitan <tristan.nowacki@outlook.com>
2024-01-12 15:48:55 -0800

    Cooldown Async Task

---
+++ Source/Aura/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,43 @@
+// Copyright CrimsonAzure
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "ActiveGameplayEffectHandle.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCooldownChange.generated.h"
+
+struct FGameplayEffectSpec;
+class UAbilitySystemComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangeSignature, float, TimeRemaining);
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownStart;
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownEnd;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UWaitCooldownChange* WaitForCooldownChange(UAbilitySystemComponent* InAbilitySystemComponent, const FGameplayTag& InCooldownTag);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	FGameplayTag CooldownTag;
+
+	void CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount);
+	void OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle);
+};
