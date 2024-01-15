a4fa6377775d9498beb6e342b81b4cd0f5e9576d
Soccertitan <tristan.nowacki@outlook.com>
2024-01-15 14:29:05 -0800

    Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,27 @@
+// Copyright CrimsonAzure
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 InExp)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+
+	while (bSearching)
+	{
+		// LevelUpInfo[1] = Level 1 Information
+		// LevelUpInfo[2] = Level 2 Information
+		if (LevelUpInfo.Num() - 1 <= Level) return Level;
+
+		if (InExp >= LevelUpInfo[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
