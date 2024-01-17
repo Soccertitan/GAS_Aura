1cbe8ee4a426132dba1bfb645e6e8ad1fec96682
Soccertitan <tristan.nowacki@outlook.com>
2024-01-16 16:05:33 -0800

    Showing XP in the HUD

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -12,6 +12,7 @@
 #include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Aura/AuraLogChannels.h"
 #include "Interaction/CombatInterface.h"
+#include "Interaction/PlayerInterface.h"
 #include "Player/AuraPlayerController.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
@@ -212,7 +213,12 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 	{
 		const float LocalIncomingXP = GetIncomingXP();
 		SetIncomingXP(0.f);
-		UE_LOG(LogAura, Log, TEXT("Incoming XP: %f"), LocalIncomingXP);
+		
+		//TODO: See if we should level up
+		if (Props.SourceCharacter->Implements<UPlayerInterface>())
+		{
+			IPlayerInterface::Execute_AddToXP(Props.SourceCharacter, LocalIncomingXP);
+		}
 	}
 }
 
---
+++ Source/Aura/Private/Interaction/PlayerInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright CrimsonAzure
+
+
+#include "Interaction/PlayerInterface.h"
+
+// Add default functionality here for any IPlayerInterface functions that are not pure virtual.
