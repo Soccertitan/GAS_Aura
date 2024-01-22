b7cd08f50ce40d14d3fae1da123cf986ad6fd3d8
Soccertitan <tristan.nowacki@outlook.com>
2024-01-22 13:29:48 -0800

    Ability Status And Type

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -62,8 +62,18 @@ public:
 
 	FGameplayTag Abilities_Attack;
 
-	FGameplayTag Abilities_Fire_FireBolt;
+	FGameplayTag Abilities_HitReact;
 
+	FGameplayTag Abilities_Status_Locked;
+	FGameplayTag Abilities_Status_Eligible;
+	FGameplayTag Abilities_Status_Unlocked;
+	FGameplayTag Abilities_Status_Equipped;
+
+	FGameplayTag Abilities_Type_Offensive;
+	FGameplayTag Abilities_Type_Passive;
+	FGameplayTag Abilities_Type_None;
+
+	FGameplayTag Abilities_Fire_FireBolt;
 	FGameplayTag Cooldown_Fire_FireBolt;
 
 	FGameplayTag Montage_Attack_Weapon;
